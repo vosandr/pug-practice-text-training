6218da40e9a0ed35f6ad9ca7f089fece1b085446
Andrey <vosirandr@gmail.com>
2023-04-21 10:55:33 +0300

    add public folder and add subtitle

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express();
 const port = 8080
 
 app.set("view engine", "pug");
-//app.set("views", "templates"); // установка пути к представлениям. По умолчанию - views
- 
+app.set("views"); // установка пути к представлениям. По умолчанию - views
+
+
 app.use("/", function(request, response){
-      
-    response.render("index");
+    response.render('index');
+
 }); 
- 
+app.use(express.static(path.join(__dirname, 'views')));
+
 app.listen(port, () => console.log(`http://localhost:${port}`));
